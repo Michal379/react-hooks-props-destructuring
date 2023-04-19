71f9982323bfb1f43dce033aedb192089eca01f3
Michal <khakaimic4@gmail.com>
2023-04-19 07:29:14 +0300

    completed code along

--- src/components/App.js
+++ src/components/App.js
@@ -14,4 +14,27 @@ function App() {
   );
 }
 
+// destructure nested objects
+function App() {
+  const socialLinks = {
+    github: "https://github.com/liza",
+    linkedin: "https://www.linkedin.com/in/liza/",
+  };
+
+  return (
+    <div>
+      <SocialMedia links={socialLinks} />
+    </div>
+  );
+}
+
+function SocialMedia({ socialLinks }) {
+  return (
+    <div>
+      <a href={socialLinks.github}>{socialLinks.github}</a>
+      <a href={socialLinks.linkedin}>{socialLinks.linkedin}</a>
+    </div>
+  );
+}
+
 export default App;
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+//parent component
 function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
@@ -13,5 +13,27 @@ function MovieCard({
     </div>
   );
 }
+// child componednt
+//without destructuring
+function MovieCard(props) {
+  return (
+    <div className="movie-card">
+      <img src={props.posterSrc} alt={props.title} />
+      <h2>{props.title}</h2>
+      <small>{props.genres.join(", ")}</small>
+    </div>
+  );
+}
+// With Destructuring
+function MovieCard({ title, posterSrc, genres }) {
+  return (
+    <div className="movie-card">
+      <img src={posterSrc} alt={title} />
+      <h2>{title}</h2>
+      <small>{genres.join(", ")}</small>
+    </div>
+  );
+}
+
 
 export default MovieCard;
